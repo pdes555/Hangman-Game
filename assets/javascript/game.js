c90cab7f65f7621638ce3454bffeaee98de18aa8
pdes555 <philld5@yahoo.com>
2017-12-06 13:13:04 -0500

    started working

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// when player clicks "click here" randomly picks a movie
+
+// sets hangman
+
+// sets new instructions
+
+// sets answer field
+
+// displays wrong guesses
+
+const movies = ["predator","the terminator","conan the barbarian","true lies","commando","total recall","last action hero","the running man","twins","jingle all the way"];
+let randMovie = movies[Math.floor(Math.random() * movies.length)];
